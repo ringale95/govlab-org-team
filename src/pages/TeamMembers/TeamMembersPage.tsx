3b4a146ffa76852a8310ad78bc74f855525206ea
Raveena Ingale <ingale.r@northeastern.edu>
2024-07-26 13:55:07 -0400

    fix: comments

--- src/pages/TeamMembers/TeamMembersPage.tsx
+++ src/pages/TeamMembers/TeamMembersPage.tsx
@@ -6,17 +6,30 @@ import { TeamMember } from "../../models/TeamMember";
 import TeamService from "../../services/TeamService";
 import { Footer } from "../../components/Footer/Footer";
 
+/**
+ * Page component that displays a list of team members.
+ * Handles loading state, error handling, and menu toggling.
+ * @returns {JSX.Element} The rendered TeamMembersPage component.
+ */
 export const TeamMembersPage = () => {
   const [teamMembers, setTeamMembers] = useState<TeamMember[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [menuActive, setMenuActive] = useState(false);
 
+  /**
+   * Toggles the menu state.
+   * @param {MouseEvent} e - The mouse event object.
+   */
   const toggleMenu: MouseEventHandler<HTMLDivElement> = (e) => {
     e.stopPropagation();
     setMenuActive(!menuActive);
   };
 
+  /**
+   * Fetches team members from the TeamService and updates the state.
+   * Handles errors and updates the loading state.
+   */
   const loadTeamMembers = async () => {
     try {
       const members = await TeamService.getTeamMembers();
@@ -28,6 +41,12 @@ export const TeamMembersPage = () => {
     }
   };
 
+  /**
+   * Custom sorting function for team members.
+   * Special names are given priority in the sorting order.
+   * @param {TeamMember[]} members - The array of team members to be sorted.
+   * @returns {TeamMember[]} The sorted array of team members.
+   */
   const customSort = (members: TeamMember[]): TeamMember[] => {
     return members.sort((a, b) => {
       const specialNames = ["BETH SIMONE NOVECK", "STEFAAN VERHULST"];
@@ -52,6 +71,7 @@ export const TeamMembersPage = () => {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
+
   return (
     <>
       <div onClick={() => setMenuActive(false)} className="main-container">
@@ -59,7 +79,7 @@ export const TeamMembersPage = () => {
         <main>
           <div className="team-member-container">
             {teamMembers.map((member) => (
-              <TeamMemberCard {...member} />
+              <TeamMemberCard key={member.id} {...member} />
             ))}
           </div>
         </main>
